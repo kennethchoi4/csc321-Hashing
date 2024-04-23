adc72859c26158467310d89a46fef82c0202b1c8
Kenneth Choi <kchoi21@calpoly.edu>
2024-04-23 10:30:09 -0700

    task1a

--- encrypt.py
+++ encrypt.py
@@ -1,8 +1,12 @@
-from Crypto.Cipher import Hash
+from Crypto.Hash import SHA256
 
 def task1():
+    hash = SHA256.new()
     user_input = input("Enter a string to hash: ")
-    return 0
+
+    # update the hash with the user input converted to bytes
+    hash.update(user_input.encode())
+    print(hash.hexdigest())
 
 
 if __name__ == "__main__":
